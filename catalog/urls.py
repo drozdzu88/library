90f8820b3b76db0156fa437064c626350fdea2d5
drozdzu88 <drozdzu88@gmail.com>
2021-05-06 19:13:05 +0200

    Added list and detail templates for books and authors

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from catalog.views import BookListView, BookDetailView
+from catalog.views import BookListView, BookDetailView, AuthorListView, \
+    AuthorDetailView
 
 #app_name = 'catalog'
 urlpatterns = [
     path('', BookListView.as_view(), name="books"),
     path('catalog/<int:pk>/', BookDetailView.as_view(), name="book-detail"),
+    path('author/', AuthorListView.as_view(), name="authors"),
+    path('author/<int:pk>/', AuthorDetailView.as_view(), name="author-detail"),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,6 +1,6 @@
 
 from django.views import generic
-from catalog.models import Book
+from catalog.models import Book, Author
 
 
 class BookListView(generic.ListView):
@@ -10,3 +10,12 @@ class BookListView(generic.ListView):
 
 class BookDetailView(generic.DetailView):
     model = Book
+
+
+class AuthorListView(generic.ListView):
+    model = Author
+    paginate_by = 20
+
+
+class AuthorDetailView(generic.DetailView):
+    model = Author
